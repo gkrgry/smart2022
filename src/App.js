e8aa5f441b79d3b14bcc6652ba11fc462a926003
gkrgry <79895925+gkrgry@users.noreply.github.com>
2022-03-10 15:16:56 +0900

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          한글로 바꿔 주세용 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
