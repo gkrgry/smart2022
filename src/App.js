f822cfa0eee35f6994727cdc5c49d06c142a6ea2
gkrgry <79895925+gkrgry@users.noreply.github.com>
2022-03-10 15:48:36 +0900

    flower

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import flower from './flower.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={flower} className="App-logo" alt="logo" />
         <p>
           한글로 바꿔 주세용 <code>src/App.js</code> and save to reload.
         </p>
