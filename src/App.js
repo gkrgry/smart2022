ddf666d1e0ae99c8348d1f35f5e262070d863ef3
gkrgry <79895925+gkrgry@users.noreply.github.com>
2022-03-17 15:38:25 +0900

    netlify upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import faker from '@faker-js/faker'; //영어
 import faker_ko from '@faker-js/faker/locale/ko';
 import './App.css';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
 
 function App() {
   const userDatas = []
@@ -16,14 +22,33 @@ function App() {
   }
 
   const userCards = userDatas.map((userData, idx) => {
+    return <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={userData.avatar}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            이름 : {userData.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            직업은 : {userData.jobName}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
 
-    return <div key={idx}>
-    <h4>{userData.name}</h4>
-    <img src={userData.avatar} alt="페이커 이미지 입니다."></img>
-    <h5>{userData.email}</h5>
-        {userData.phoneNo} <br/>
-        {userData.jobName}
-    </div>
+
+    // return <div key={idx}>
+    // <h4>{userData.name}</h4>
+    // <img src={userData.avatar} alt="페이커 이미지 입니다."></img>
+    // <h5>{userData.email}</h5>
+    //     {userData.phoneNo} <br/>
+    //     {userData.jobName}
+    // </div>
   })
 
 
