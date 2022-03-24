15adcd784794d3ad77a2b007efc7446767269f0f
gkrgry <79895925+gkrgry@users.noreply.github.com>
2022-03-24 16:12:44 +0900

    pagination

--- src/App.js
+++ src/App.js
@@ -8,11 +8,19 @@ import Switch from '@mui/material/Switch';
 import {ThemeProvider, createTheme} from '@mui/material/styles';
 import { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
+import Pagination from '@mui/material/Pagination';
 const userDatas = []
 
-  while(userDatas.length < 5){
+const getRandomIntInclusive = (min, max) => {
+  min = Math.ceil(min);//ceil 올림처리
+  max = Math.floor(max);//floor 내림처리
+  return Math.floor(Math.random() * (max - min) + min);//random 0~0.999.... 
+  }
+
+
+  while(userDatas.length < 5000){
     userDatas.push({
-      avatar: faker.image.avatar(),
+      avatar: `images/${getRandomIntInclusive(1,5)}.jpg`,
       name: `${faker.name.lastName()} ${faker_ko.name.firstName()}`,
       email: faker.internet.email(),
       jobName: faker.name.jobTitle(),
@@ -20,17 +28,32 @@ const userDatas = []
     })
   }
 
+  const paginate = (array, pageSize, pageNumber) => {
+    return array.slice((pageNumber - 1) * pageSize, pageNumber * pageSize);
+    }
 
 function App() {
+  const pageCountentCount = 9;
+
+
   // props  부모에게 받아서 쓰는것
   // state 나 자기 자신에서 선언해서 쓰는것
   const [useDarkMode, setUseDarkMode] = useState(true); //디폴트가 true
-  const [count, setCount] = useState(0);
+  const [pageNo, setPageNo] = useState(1);
+  const [currentUserData, setCurrentUserData] = useState(paginate(userDatas, pageCountentCount, pageNo));
+
+
   const handleChange = (event) => {
     console.log(event);
     setUseDarkMode(useDarkMode? false:true);
     //setUseDarkMode(event.target.checked) 위와 같음
   }
+  const handleChangePageNo = (event, value) => {
+    setPageNo(value);
+    setCurrentUserData(paginate(userDatas, pageCountentCount, value))
+  }
+
+
   //생명주기와 관련된 함수
   //아래 배열이 없으면 최초에 한번만 세팅
   useEffect(() => {
@@ -41,11 +64,15 @@ function App() {
     console.log("render");
   },[useDarkMode])
   
-  const userCards = userDatas.map((userData, idx) => {
+
+  const userCards = currentUserData.map((userData, idx) => {
     return <Grid item xm={2} sm={2} md={4}>
       <UserCard userData={userData} idx={idx} />
       </Grid>
   })
+
+
+
   return (
    
     <ThemeProvider theme={createTheme({
@@ -68,12 +95,17 @@ function App() {
 
       </Switch>
       <Container maxWidth="lg" sx={{p:1}}>
-      <Grid container spacing={{xs:2, md: 3}} columns={{xs: 4, sm: 8, md: 12}}>
+        <Grid container spacing={{xs:2, md: 3}} columns={{xs: 4, sm: 8, md: 12}}>
         {userCards}
-      </Grid>
-    </Container>
+        </Grid>
+        <Pagination 
+        color="primary" 
+        count={Math.ceil(userDatas.length / pageCountentCount)}
+        page={pageNo}
+        onChange={handleChangePageNo}
+        />
+      </Container>
     </Box>
-  
   </ThemeProvider>
   );
 }
