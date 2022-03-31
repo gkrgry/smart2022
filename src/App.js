39e6aa50c8dd06990049db3ddeffeb08b3ef8e39
gkrgry <79895925+gkrgry@users.noreply.github.com>
2022-03-31 17:05:15 +0900

    api 쓰기

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import './App.css';
 import Container from '@mui/material/Container';
 import Switch from '@mui/material/Switch';
 import {ThemeProvider, createTheme} from '@mui/material/styles';
 import { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
-import Pagination from '@mui/material/Pagination';
 import { makeUserDatas } from './Utils';
 import UserCardList from './components/UserCardList';
+import axios from 'axios';
+import WeatherCard from './components/WeatherCard';
 
 const userDatas = makeUserDatas(5000);
 
- 
+
+
+
+
+// axios.get("https://api.openweathermap.org/data/2.5/weather?lat=37.391109&lon=126.967785&lang=kr&units=metric&appid=e3e4cfbebd376feeec6e33c75454a739")
+// .then((res) => {
+//   console.log(res)
+// }).catch((error)=>{
+//   console.log(error)
+// })
 
 function App() {
+
+
   const [useDarkMode, setUseDarkMode] = useState(true); //디폴트가 true
       // props  부모에게 받아서 쓰는것
     // state 나 자기 자신에서 선언해서 쓰는것
+
+    const [weatherData, setWeatherData] = useState(null);
+    const [apiError, setApiError] = useState(null);
+
 
 
   const handleChange = (event) => {
@@ -23,6 +38,23 @@ function App() {
     setUseDarkMode(useDarkMode? false:true);
     //setUseDarkMode(event.target.checked) 위와 같음
   }
+
+
+
+  useEffect(() => {
+    const callApi = async () => {
+      try {
+        const result = await axios.get(
+          'https://api.openweathermap.org/data/2.5/weather?lat=37.3943&lon=126.9568&lang=kr&units=metric&&appid=e3e4cfbebd376feeec6e33c75454a739',
+        );
+        setWeatherData(result.data);
+      } catch (err) {
+        setApiError(err);
+      }
+    };
+    callApi();
+    console.log('component did mount');
+  }, []);
 
 
   //생명주기와 관련된 함수
@@ -36,6 +68,7 @@ function App() {
   },[useDarkMode])
   
 
+ 
 
 
 
@@ -48,6 +81,13 @@ function App() {
       },
     })
   }>
+    <Box sx={{
+            bgcolor: 'background.default',
+            color: 'text.primary',
+            p: 1,
+            }}>
+        <WeatherCard weatherData={weatherData} apiError={apiError}/>
+      </Box>
     <Box sx={{
     height: '100%',
     bgcolor: 'background.default',
